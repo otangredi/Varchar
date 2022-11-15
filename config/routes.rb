eea2bbdbf581d281936048e47c0fcc93c7bd45b4
anthonyborremans <anthonyborremans@hotmail.com>
2022-11-15 16:51:59 -0500

    add routes bookings host

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,7 @@ Rails.application.routes.draw do
   delete '/bookings/:id', to: "bookings#destroy", as: :delete_booking
   get '/my-bookings', to: "bookings#my_bookings"
   get '/my-characters', to: "characters#my_characters"
+  get '/host-bookings/:id', to: "bookings#host_booking"
+  patch '/host-bookings/:id/set-status', to: "bookings#set_status"
+  get '/host-bookings', to: "bookings#host_bookings"
 end
