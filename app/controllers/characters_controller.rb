7ee8206ec0ec73b63875f99bf0206a1eeb309043
Olivia Tangredi oliviatangredixo@gmail.com <oliviatangredixo@gmail.com>
2022-11-15 15:49:55 -0500

    add new character method created

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -9,6 +9,21 @@ class CharactersController < ApplicationController
     @booking = Booking.new
   end
 
+  def new
+    @character = Character.new
+  end
+
+  def create
+    @character = Character.new(character_params)
+    @character.user = current_user
+
+    if @character.save
+      redirect_to @character, notice: 'Character was successfully created!'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
   def my_characters
     @characters = Character.where(user: current_user)
   end
@@ -17,5 +32,9 @@ class CharactersController < ApplicationController
 
   def set_character
     @character = Character.find(params[:id])
+  end
+
+  def character_params
+    params.require(:character).permit(:name, :description, :price, :category, :user_id)
   end
 end
