64b232852dcc37f11a41e69d0c6fada57465f658
bieeeee <114767772+bieeeee@users.noreply.github.com>
2022-11-15 11:23:54 -0500

    Revert "created bookings controller with some actions"

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  resources :bookings
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
 end
