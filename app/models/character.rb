1f36bb4e8ee69f26ba1d4447b9b6effc0ea3863a
Olivia Tangredi oliviatangredixo@gmail.com <oliviatangredixo@gmail.com>
2022-11-17 13:59:22 -0500

    minor changes made

--- app/models/character.rb
+++ app/models/character.rb
@@ -6,6 +6,7 @@ class Character < ApplicationRecord
   validates :price, :description, :category, :address, presence: true
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
+
   include PgSearch::Model
   pg_search_scope :search_by_name_and_description,
     against: [ :name, :description ],
